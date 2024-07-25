2191bf6f27eb5e9ed9b861ec3d3c042486d2a2fa
adyatma-ID <50876823+adyatma200@users.noreply.github.com>
2024-07-25 12:23:13 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    // JavaScript can be added here if needed
+});
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+ var emailButton = document.getElementById('emailButton');
+
+ emailButton.addEventListener('click', function() {
+  window.location.href = 'mailto:adiatma@example.com';
+ });
+});
